25f192eae6cce76667aac935ef3f6d12765cabd4
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-11-04 11:51:41 -0500

    Forgot to push AppContext - now fingerprints should work

--- client/src/AppContext.js
+++ client/src/AppContext.js
@@ -1,33 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { authRefresh } from './store/actions/authActions'
+import { authRefresh, authFingerprint } from './store/actions/authActions'
 import LoadingOverlay from 'react-loading-overlay';
 import { RotateSpinner } from 'react-spinners-kit'
 
 
+
 export default ({ children }) => {
 
-	// TODO : loading msg
-	const global_loading = useSelector(state => state.auth.global_loading)
-	const auth_loading = useSelector(state => state.auth.loading)
-	const poll_loading = useSelector(state => state.poll.loading)
-
-	const [load, setLoad] = useState(undefined);
 	const dispatch = useDispatch();
 
-	const loading = !!auth_loading || !!poll_loading || !!global_loading;
+	const { loading: auth_loading, global_loading, fingerprint } = useSelector(state => state.auth)
+	const { loading: poll_loading } = useSelector(state => state.poll)
+
+	// Reduce loading state to a single boolean
+	const _loading = !!auth_loading || !!poll_loading || !!global_loading || !fingerprint;
 
 	// On first load only
 	useEffect(() => {
 		const refresh = localStorage.getItem('refresh');
-		if (refresh) dispatch(authRefresh(refresh))
 
-	}, [load])
+		// TODO : might need to perferom auth
+		// TODO : ONLY AFTER getting fingerprint
+		// Get fingerprint
+		if (!fingerprint) dispatch(authFingerprint());
+
+		// Refresh auth token
+		if (refresh) dispatch(authRefresh(refresh));
+
+	}, [])
 
 	return (
 		<>
 			<LoadingOverlay
-				active={loading}
+				active={_loading}
 				spinner={<RotateSpinner size={80} color={'#55c57a'} />}
 				text='Loading stuff'
 			>
