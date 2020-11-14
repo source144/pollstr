2a8b7358515d49a4179c85f82eb367a776f2c000
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-11-14 05:31:14 -0500

    Added tool/action buttons to Poll and its styling

--- client/src/components/pages/Poll/Poll.js
+++ client/src/components/pages/Poll/Poll.js
@@ -5,6 +5,7 @@ import CountdownTimer from '../../CountdownTimer';
 // TODO : Create my own react-hashtag component
 import ReactHashtag from "react-hashtag";
 import { useSelector, useDispatch } from 'react-redux';
+import QRCode from 'qrcode.react';
 
 import moment from 'moment';
 
@@ -71,6 +72,8 @@ const Poll = () => {
 
 	// TODO : get ID from path
 	const { id } = useParams();
+	const [modalContent, setModalContent] = useState(undefined);
+	const [minimized, setMinimized] = useState(false);
 	const [passcode, setPasscode] = useState(undefined);
 	const pollWrapper = useRef();
 	const passcodeInput = useRef();
@@ -84,8 +87,8 @@ const Poll = () => {
 	const dispatch = useDispatch();
 	const { modalProps, open, close } = useModal({
 		id: 'passcode-modal',
-		onClose: () => dispatch(modalClose()),
-		onOpen: () => passcodeInput.current.focus(),
+		onClose: () => dispatch(modalClose(pollWrapper)),
+		onOpen: () => { if (passcodeInput && passcodeInput.current) passcodeInput.current.focus() },
 		background: 'rgb(0, 0, 0, 0.6)'
 	});
 
@@ -96,7 +99,8 @@ const Poll = () => {
 		if (!poll) return;
 
 		if (poll.passcode) {
-			dispatch(modalOpen());
+			setModalContent("PASSCODE");
+			dispatch(modalOpen(pollWrapper));
 			open(pollWrapper);
 		}
 		else dispatch(votePoll(id, selected))
@@ -112,6 +116,16 @@ const Poll = () => {
 		close(pollWrapper);
 	}
 
+	const handleShare = (e) => {
+		e.preventDefault();
+		if (!poll) return;
+
+		setModalContent("QR");
+		dispatch(modalOpen(pollWrapper));
+		open(pollWrapper);
+	}
+
+	// TODO : useEffect for error to display error toasts
 	// Fetch poll ONLY after 
 	// identifying user/guest
 	useEffect(() => {
@@ -121,14 +135,60 @@ const Poll = () => {
 		}
 	}, [id, _prevent_fetch_, dispatch]);
 
-	// TODO : useEffect for error to display error toasts
+	const _passcodeModal = <div className="form-centered-container">
+		<div className="form-form-wrapper">
+			<h1 className='form-title'>Enter Passcode</h1>
+			<form onSubmit={handlePasscode} formNoValidate className='form-form'>
+				<div className="form-item">
+					<div className='form-item-wrapper'>
+						<input
+							value={passcode ?? ''}
+							className='form-item__input'
+							type="password"
+							placeholder="e.g. ********"
+							name="passcode"
+							formNoValidate
+							onChange={(e) => setPasscode(e.target.value)}
+							ref={passcodeInput} />
+						<span className='form-item__input-icon'><i className="fas fa-lock"></i></span>
+					</div>
+				</div>
+				<div className="form-item">
+					<input
+						disabled={!passcode || !passcode.trim()}
+						onClick={handlePasscode}
+						className='btn btn--tertiary form-item__submit'
+						type="button" value="Vote" />
+				</div>
+			</form>
+		</div>
+	</div>
+
+	const _QRModal = <div className="form-centered-container">
+		<div className="form-form-wrapper">
+			<div onSubmit={(e) => { e.preventDefault() }} formNoValidate className='form-form'>
+				<div className="form-switch poll-created-description">Use this QR Code to Acces Poll</div>
+				<div className="poll-created-qr">
+					<QRCode value={`${window.location.protocol}//${window.location.host}/poll/${poll.id}`} size={200} />
+				</div>
+				<div className="form-switch poll-created-description">You can also use <a to={`/poll/${poll.id}`} className='form-switch-action'>This Link</a></div>
+			</div>
+		</div>
+	</div>
+
+	let _modal_content;
+	switch (modalContent) {
+		case "PASSCODE": _modal_content = _passcodeModal; break;
+		case "QR": _modal_content = _QRModal; break;
+		default: _modal_content = undefined; break;
+	}
 
 	return (
 		<>
 			<div className="form-centered-container">
 				{!_prevent_fetch_ && !poll_loading && poll && Object.keys(poll).length > 2 ?
 					<div className="form-form-wrapper poll-wrapper" ref={pollWrapper}>
-						<div className="poll-detail-wrapper" style={isMobile && poll.timeToLive ? {padding: "2rem"} : {}}>
+						<div className="poll-detail-wrapper" style={isMobile && poll.timeToLive ? { padding: "2rem" } : {}}>
 							{!poll.timeToLive ? undefined :
 								<CountdownTimer
 									onComplete={() => { dispatch(disableVoting()) }}
@@ -140,64 +200,58 @@ const Poll = () => {
 								<ul className='poll-tags'>{poll.tags.map((tag, i) => tag ? <Chip key={i}>{tag}</Chip> : null)}</ul>
 								<span className="poll-total-votes">{`${poll.total_votes > 0 ? poll.total_votes : 'no'} voter${poll.total_votes != 1 ? 's' : ''}`}</span>
 							</div>
+							<div className="poll-actions">
+								<div className="poll-action poll-action--share" onClick={handleShare}>
+									<i className="fas fa-link"></i>
+								</div>
+								{/* <div className="poll-action poll-action--share" onClick={() => { }}>
+								<i className="fas fa-share-alt"></i>
+							</div> */}
+								{/* <div className="poll-action poll-action--edit" onClick={() => { }}>
+									<i className="fas fa-pencil-alt"></i>
+								</div>
+								<div className="poll-action poll-action--delete" onClick={() => { }}>
+									<i className="fas fa-trash-alt"></i>
+								</div>
+								<div className="poll-action poll-action--minimize" onClick={() => setMinimized(!minimized)}>
+									{minimized ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
+								</div> */}
+							</div>
 						</div>
-						<div className="form--mb1"></div>
-						<div className="form--mb1"></div>
+						<div className="poll-body">
+							<div className="form--mb1"></div>
+							<div className="form--mb1"></div>
 
-						{poll.options.map((option) =>
-							<PollOption
-								key={option.id}
-								option={option}>
-							</PollOption>)
-						}
-						{/* {options.map(option => <PollOption id={option.id} title={option.title} description={option.description} votes={option.votes} percent={option.percent} voted={option.voted}></PollOption>)} */}
-						{/* <PollOption title={options[0].title} description={options[0].description} percent={options[0].percent}></PollOption>
+							{poll.options.map((option) =>
+								<PollOption
+									key={option.id}
+									option={option}>
+								</PollOption>)
+							}
+							{/* {options.map(option => <PollOption id={option.id} title={option.title} description={option.description} votes={option.votes} percent={option.percent} voted={option.voted}></PollOption>)} */}
+							{/* <PollOption title={options[0].title} description={options[0].description} percent={options[0].percent}></PollOption>
 				<PollOption title={options[1].title} description={options[1].description} percent={options[1].percent}></PollOption>
 				<PollOption title={options[2].title} description={options[2].description} percent={options[2].percent}></PollOption> */}
-						{error ? <div className="form-item form-item__error">{error}</div> : null}
-						<div className="form-item">
-							<input
-								className="btn btn--tertiary form-item__submit"
-								type="submit" value="Vote" onClick={handleVote}
-								disabled={selected == null || poll.voted != null || poll.expired} />
+							{error ? <div className="form-item form-item__error">{error}</div> : null}
+							<div className="form-item">
+								<input
+									className="btn btn--tertiary form-item__submit"
+									type="submit" value="Vote" onClick={handleVote}
+									disabled={selected == null || poll.voted != null || poll.expired} />
+							</div>
 						</div>
+
+						<Modal {...modalProps}>
+							{_modal_content}
+						</Modal>
 					</div>
 					// : error ? <h1>{error}</h1> : <div style={{ height: "100%", width: "100%" }}></div>
 					: error ? <h1>{error}</h1> : <Placeholder />
 					// : error ? <h1>{error}</h1> : <__poll_placeholder />
 					// __poll_placeholder()
 				}
+
 			</div>
-			<Modal {...modalProps}>
-				<div className="form-centered-container">
-					<div className="form-form-wrapper">
-						<h1 className='form-title'>Enter Passcode</h1>
-						<form onSubmit={() => { }} formNoValidate className='form-form'>
-							<div className="form-item">
-								<div className='form-item-wrapper'>
-									<input
-										value={passcode ?? ''}
-										className='form-item__input'
-										type="password"
-										placeholder="e.g. ********"
-										name="passcode"
-										formNoValidate
-										onChange={(e) => setPasscode(e.target.value)}
-										ref={passcodeInput} />
-									<span className='form-item__input-icon'><i className="fas fa-lock"></i></span>
-								</div>
-							</div>
-							<div className="form-item">
-								<input
-									disabled={!passcode || !passcode.trim()}
-									onClick={handlePasscode}
-									className='btn btn--tertiary form-item__submit'
-									type="button" value="Vote" />
-							</div>
-						</form>
-					</div>
-				</div>
-			</Modal>
 		</>
 	);
 
