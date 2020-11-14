5a7a8e2b2289f06055d4cd2b8d7667e92d125716
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-11-14 05:29:31 -0500

    Modal revamp close animation, positioning & disabled scrolling.
    Might need refactoring

--- client/src/store/actions/modalActions.js
+++ client/src/store/actions/modalActions.js
@@ -3,43 +3,75 @@ import { MODAL_CLOSE, MODAL_OPEN, MODAL_FADE } from './types/modalTypes'
 let modalCloseBtnEvnt;
 let modalKeyPressEvnt;
 let placeholder;
+let container;
 let closebtn;
-export const modalOpen = () => {
-	setTimeout(() => {
-		placeholder = document.querySelector('.RMM__container.RMM__container--is-active');
-		closebtn = document.querySelector('.RMM__container .RMM__close-button')
 
-		if (closebtn && placeholder) {
+export const modalOpen = (ref = undefined) => {
+	setTimeout(() => {
+		container = document.querySelector('.RMM__container.RMM__container--is-active');
+		placeholder = document.querySelector('.RMM__container.RMM__container--is-active .RMM__placeholder');
+
+		if (ref && ref.current) closebtn = ref.current.querySelector('.RMM__container .RMM__close-button');
+		else closebtn = document.querySelector('.RMM__container .RMM__close-button');
+
+		console.log('closebtn', closebtn);
+
+		// Disable scrolling while modal is open
+		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+		window.onscroll = function () { window.scrollTo(scrollLeft, scrollTop); };
+
+		if (ref && ref.current && placeholder) {
+			const rect = ref.current.getBoundingClientRect();
+			console.log(`${Math.floor(rect.top)}px - ${Math.floor(rect.left)}px`)
+			placeholder.style.top = `${Math.floor(rect.top)}px`;
+			placeholder.style.left = `${Math.floor(rect.left)}px`;
+		}
+
+		if (closebtn && container) {
 			console.log(closebtn)
 			if (modalCloseBtnEvnt) closebtn.removeEventListener('click', modalCloseBtnEvnt);
 			modalCloseBtnEvnt = function () {
-				if (placeholder)
-					placeholder.classList.add("opacity-0");
+				console.log('Close btn clicked')
+				if (container)
+					container.classList.add("opacity-0");
 			}
 			modalKeyPressEvnt = function (e) {
-				if (placeholder && e.key === "Escape")
-					placeholder.classList.add("opacity-0");
+				if (container && e.key === "Escape")
+					container.classList.add("opacity-0");
 			}
 
 			document.body.addEventListener('keydown', modalKeyPressEvnt)
 			closebtn.addEventListener('click', modalCloseBtnEvnt);
 		}
 
-		console.log(placeholder)
+		console.log(container)
 		console.log(closebtn)
 	}, 1)
 
 	return ({ type: MODAL_OPEN })
 }
 
-export const modalClose = () => {
+export const modalClose = (ref = undefined) => {
 	console.log(closebtn)
-	console.log(placeholder)
+	console.log(container)
+	placeholder = document.querySelector('.RMM__container.RMM__container--is-active .RMM__placeholder');
+
+	// Enable scrolling
+	window.onscroll = function () { };
+
+
+	if (ref && ref.current && placeholder) {
+		const rect = ref.current.getBoundingClientRect();
+		console.log(`close to ${Math.floor(rect.top)}px - ${Math.floor(rect.left)}px`)
+		placeholder.style.top = `${Math.floor(rect.top)}px`;
+		placeholder.style.left = `${Math.floor(rect.left)}px`;
+	}
 
 	if (closebtn) {
 		closebtn.removeEventListener('click', modalCloseBtnEvnt);
 		document.body.removeEventListener('keydown', modalKeyPressEvnt);
-		placeholder = closebtn = modalCloseBtnEvnt = modalKeyPressEvnt = undefined;
+		container = closebtn = modalCloseBtnEvnt = modalKeyPressEvnt = undefined;
 	}
 
 	return ({ type: MODAL_CLOSE });
