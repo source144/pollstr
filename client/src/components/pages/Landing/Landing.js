a922f1860e323b59e57be56bcc281b35a33b46b4
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-12-03 05:09:37 -0500

    auto import missed

--- client/src/components/pages/Landing/Landing.js
+++ client/src/components/pages/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { authLogout } from '../../../store/actions/authActions';
